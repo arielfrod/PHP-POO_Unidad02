8700cb0c74ea5448461ce280c8ad43fe035faaf8
arielfrod <arielfrod@gmail.com>
2021-07-26 21:35:42 -0300

    Poo Unidad02

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-POO Unidad 02</title>
+</head>
+
+<body align="center">
+    <h1 align="center">Bienvenidos al Sistema </h1>
+    <br>
+    <br>
+
+
+
+    <?php
+
+
+
+    $nombre = "Ariel";
+    $apellido = "Rodriguez";
+    $edad = 43;
+    $hobbie = "Jugar al paddle";
+    $editor = "Visual Studio Code";
+    $sistema_operativo = "Windows 10";
+
+
+
+
+    echo "<b style= margin-left:-70px;>Nombre:</b> <b>$nombre</b>  <br>";
+    echo "<b style= margin-left:-75px;>Apellido:</b> <b>$apellido</b> <br>";
+    echo "<b style= margin-left:-125px;>Edad:</b> <b>$edad</b> <br>";
+    echo " <b style= margin-left:-28px;>Hobbie:</b> <b>$hobbie</b> <br> ";
+    echo "<b style= margin-left:130px;>Editor de código preferido:</b> <b>$editor</b> <br> ";
+    echo "<b style= margin-left:25px;>Sistema operativo:</b> <b>$sistema_operativo</b>";
+
+
+    ?>
+</body>
+
+</html>
